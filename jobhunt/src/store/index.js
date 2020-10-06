ac5a039b62aaadedaec8d3b7df104cf118cc6e6c
Azusa Okamoto <curiousazunas@gmail.com>
2020-10-06 15:10:27 +0900

    フォームの作成

--- jobhunt/src/store/index.js
+++ jobhunt/src/store/index.js
@@ -5,6 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    companies: [
+      {
+        name: '株式会社Freewill',
+        area: '東京都',
+        progress: '内定',
+        url: 'https://www.free-will.co/'
+      }
+    ]
+  },
+  getters: {
+    getCompanies() {
+      return this.$store.state.getCompanies;
+    }
   },
   mutations: {
   },
