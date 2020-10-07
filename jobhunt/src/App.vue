21b3d310fe6c1f509c394df113581ffd4eae448f
Azusa Okamoto <curiousazunas@gmail.com>
2020-10-07 18:04:37 +0900

    フォーム入力・削除機能を搭載

--- jobhunt/src/App.vue
+++ jobhunt/src/App.vue
@@ -1,17 +1,65 @@
 <template>
   <div id="app">
     <h1>就職活動状況リスト</h1>
-    <Form />
+    <form v-on:submit.prevent="onclick">
+      <label for="name">社名：</label>
+      <input type="text" v-model="name" placeholder="社名を入力して下さい" />
+      <label for="area">所在地(勤務地)：</label>
+      <input type="text" v-model="area" placeholder="所在地を入力して下さい" />
+      <label for="progress">選考状況：</label>
+      <input type="text" v-model="progress" placeholder="選考状況を入力して下さい" />
+      <label for="url">URL：</label>
+      <input type="url" v-model="url" placeholder="URLを入力して下さい" />
+      <input type="submit" value="登録" />
+    </form>
+    <ul v-for='(company, index) in companies' :key="index">
+      <li>
+        {{ company.name }}
+        {{ company.area }}
+        {{ company.progress }}
+        {{ company.url }}
+        <div>
+          <input type="button" v-on:click="deleteCompanies(company.name)" value="削除" />
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Form from './components/form.vue';
-
 export default {
   name: 'App',
-  components: {
-    Form
+  computed: {
+    companies() {
+      return this.$store.getters.companies;
+    }
+  },
+  data() {
+    return {
+      name: '',
+      area: '',
+      progress: '',
+      url: ''
+    }
+  },
+  methods: {
+    onclick() {
+      this.$store.commit('addCompanies', {
+        company: {
+          name: this.name,
+          area: this.area,
+          progress: this.progress,
+          url: this.url
+        },
+      })
+      this.name = '';
+      this.area = '';
+      this.progress = '';
+      this.url = '';
+    },
+    deleteCompanies(name) {
+      this.$store.commit("deleteCompanies", name)
+    }
   }
 }
 </script>
